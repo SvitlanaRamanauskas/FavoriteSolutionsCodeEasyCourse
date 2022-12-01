be1db9367213c311c120162f54f3fdc24ee0117b
SviatLama <ramanauskassvs@gmail.com>
2022-12-01 13:51:08 +0200

    adjusted task Spy

--- FavoriteSolutionsCodeEasyCourse/6 The truth/C sharp test/ExamTaskCatchASpy.cs
+++ FavoriteSolutionsCodeEasyCourse/6 The truth/C sharp test/ExamTaskCatchASpy.cs
@@ -9,46 +9,46 @@ namespace Exam
             double hours = double.Parse(Console.ReadLine());
             double minutes = double.Parse(Console.ReadLine());
             double cycleAngle = 360;
-            double sumDs = 0;
+
             double hourMax = 12;
             double minutesMax = 60;
-
-            double gradusPlus;
-            double differenceHoursinDs = 0;
-            double differenceMinutesinDs = 0;
-            if ((hours == 0) && ((minutes > 0) && (minutes <= 59)))
+            int oneHourInDs = 30;
+            double gradusAddToHours;
+            double differenceHoursInDs = 0;
+            double differenceMinutesInDs = 0;
+            if (hours == 0 && minutes > 0 && minutes <= 59) //time: 00:48
             {
-                gradusPlus = 0;
-                differenceHoursinDs = 0;
-                differenceMinutesinDs = (minutesMax - minutes) * 6;// (60-15)  * 6 = 270
+                gradusAddToHours = minutes * oneHourInDs / 60; // (Proporsion) 60 is amount of min in 1 hour // 24
+                differenceHoursInDs = (hourMax - hours) * oneHourInDs - gradusAddToHours; // 336
+                differenceMinutesInDs = (minutesMax - minutes) * 6;// 6 is amount of degrees in 1 minute //(60-48) * 6 = 72
             }
-            else if ((minutes == 0) && ((hours > 0) && (hours <= 12)))
+            else if (minutes == 0 && hours > 0 && hours <= 12) //time: 07:00
             {
-                gradusPlus = (minutes * 30) / 60; // 7.5 to add to hoursinDegrees M= 40*6 = 240  30=60; ?g = minutes; (m*30)/60 = gradusy+ 
-                differenceHoursinDs = (hourMax - hours) * 30 - gradusPlus;//  (12 - 7) * 30 - 7.5 = 142.5;  60
-                differenceMinutesinDs = 0;
+                gradusAddToHours = 0; // no need
+                differenceHoursInDs = (hourMax - hours) * oneHourInDs - gradusAddToHours;// (12 - 7) * 30 = 150;
+                differenceMinutesInDs = (minutesMax - minutes) * 6; //360;
             }
-            else if ((minutes == 0) && (hours == 0))
+            else if (minutes == 0 && hours == 0) // can skip but I leave for visualisation
             {
-                gradusPlus = 0;
-                differenceHoursinDs = 0;
-                differenceMinutesinDs = 0;
+                gradusAddToHours = 0;
+                differenceHoursInDs = 0;
+                differenceMinutesInDs = 0;
             }
-            else if (((hours > 0) && (hours <= 12)) && ((minutes > 0) && (minutes <= 59)))
+            else if (hours > 0 && hours <= 12 && minutes > 0 && minutes <= 59) //time: 10:57
             {
-                gradusPlus = (minutes * 30) / 60; // 28.5
-                differenceHoursinDs = (hourMax - hours) * 30 - gradusPlus;// 61.5
-                differenceMinutesinDs = (minutesMax - minutes) * 6; // 18;
-            }
-
-            sumDs = Math.Abs(differenceHoursinDs - differenceMinutesinDs);
-
-            if (sumDs > 180)
-            {
-                sumDs = cycleAngle - sumDs;
+                gradusAddToHours = minutes * oneHourInDs / 60; // 28.5  
+                differenceHoursInDs = (hourMax - hours) * oneHourInDs - gradusAddToHours; // 31.5
+                differenceMinutesInDs = (minutesMax - minutes) * 6; // 18
             }
 
-            Console.WriteLine(sumDs);
+
+            double smallestAngle = Math.Abs(differenceHoursInDs - differenceMinutesInDs);
+
+            if (smallestAngle > 180)
+            {
+                smallestAngle = cycleAngle - smallestAngle;
+            }
+            Console.WriteLine(smallestAngle);
         }
     }
 }
